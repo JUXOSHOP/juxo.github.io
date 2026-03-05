4a4224933c1e47f6b529a8dc6910709de050b7c9
JUXO <tokiodan93@gmail.com>
2026-03-04 23:42:00 -0300

    Update cache version from 'jupiter-v1' to 'jupiter-v2'

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.addEventListener('install', (e) => {
   e.waitUntil(
-    caches.open('jupiter-v1').then((cache) => {
+    caches.open('jupiter-v2').then((cache) => {
       return cache.addAll(['/', '/index.html']);
     })
   );
